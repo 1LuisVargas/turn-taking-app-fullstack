a5e6976e7e5889dd75d28abf564255fe6f66b0b3
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-05-18 18:48:27 -0500

    Added async to appointments service

--- back/src/services/appointmentsService.ts
+++ back/src/services/appointmentsService.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
 import { appointments } from "../interfaces/IAppointment";
 
-export const getAllAppointments = (_req: Request, res: Response) => {
+export const getAllAppointments = async (_req: Request, res: Response) => {
 
 };
 
-export const getAppointmentByID = (_req: Request, res: Response) => {
+export const getAppointmentByID = async (_req: Request, res: Response) => {
 
 };
 
-export const createAppointment = (_req: Request, res: Response) => {
+export const createAppointment = async (_req: Request, res: Response) => {
 
 };
 
-export const cancelAppointment = (_req: Request, res: Response) => {
+ export const cancelAppointment = async (_req: Request, res: Response) => {
 
 };
