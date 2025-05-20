eeaa739bb4230d40375b5a602c1c2ed4f52959f4
Luis Vargas <luisvargas98@hotmail.com>
2025-05-20 16:53:37 -0500

    Fixed the arguments sent in createCredential call. Formatting changes

--- back/src/services/usersService.ts
+++ back/src/services/usersService.ts
@@ -7,39 +7,45 @@ const usersDB: IUser[] = [];
 let userId: number = 1;
 
 //Service to get all users
-export const getUsers = async ():Promise<IUser[]> => {
-    return usersDB;
+export const getUsers = async (): Promise<IUser[]> => {
+  return usersDB;
 };
 
 //Service to get a user by ID
-export const getUserByID = async (id: number):Promise<IUser> => {
-    const foundUser: IUser | undefined = usersDB.find((user: IUser) => user.id === id);
-    //If the user is found, return the user, else throw an error
-    if (foundUser) {
-        return foundUser;
-    }
-    else {
-        throw new Error("User not found");
-    }
-}
+export const getUserByID = async (id: number): Promise<IUser> => {
+  const foundUser: IUser | undefined = usersDB.find(
+    (user: IUser) => user.id === id
+  );
+  //If the user is found, return the user, else throw an error
+  if (foundUser) {
+    return foundUser;
+  } else {
+    throw new Error("User not found");
+  }
+};
 
 //Service to create a new user
-export const createUser = async (createUserDTO: createUserDTO):Promise<IUser> => {
-    //Calling the service to create a new credential
-    const newCredentialID = await createCredential(createUserDTO.username, createUserDTO.email);
+export const createUser = async (
+  createUserDTO: createUserDTO
+): Promise<IUser> => {
+  //Calling the service to create a new credential
+  const newCredentialID = await createCredential(
+    createUserDTO.username,
+    createUserDTO.password
+  );
 
-    //Creating the new user
-    const newUser :IUser = {
-        id: userId,
-        name: createUserDTO.name,
-        email: createUserDTO.email,
-        birthdate: createUserDTO.birthdate,
-        Dni: Number(createUserDTO.Dni),
-        credentialsId: newCredentialID
-    }
+  //Creating the new user
+  const newUser: IUser = {
+    id: userId,
+    name: createUserDTO.name,
+    email: createUserDTO.email,
+    birthdate: createUserDTO.birthdate,
+    Dni: Number(createUserDTO.Dni),
+    credentialsId: newCredentialID,
+  };
 
-    //Pushing the new user to the DB
-    usersDB.push(newUser);
-    userId++;
-    return newUser;
-}
+  //Pushing the new user to the DB
+  usersDB.push(newUser);
+  userId++;
+  return newUser;
+};
