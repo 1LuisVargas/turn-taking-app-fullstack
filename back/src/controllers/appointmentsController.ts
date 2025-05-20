87b1efd1a318d6226395997d6975bcb3ac3dcca6
Luis Vargas <luisvargas98@hotmail.com>
2025-05-20 17:59:14 -0500

    Connected to appointments controller to work properly with the newly created services

--- back/src/controllers/appointmentsController.ts
+++ back/src/controllers/appointmentsController.ts
@@ -1,20 +1,87 @@
+// Importing modules
 import { Request, Response } from "express";
-import { getAllAppointments, getAppointmentByID, createAppointment, cancelAppointment } from "../services/appointmentsService";
+import {
+  getAllAppointments,
+  getAppointmentByID,
+  createAppointment,
+  cancelAppointment,
+} from "../services/appointmentsService";
+import { IAppointment } from "../interfaces/IAppointment";
 
 // GET
-export const getAppointmentsController = async (_req: Request, res: Response) => {
-    getAllAppointments(_req, res);
+export const getAppointmentsController = async (
+  _req: Request,
+  res: Response
+) => {
+  try {
+    const appointments: IAppointment[] = await getAllAppointments();
+    res.status(200).json({
+      success: true,
+      data: appointments,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
-export const getAppointmentByIdController = async (_req: Request, res: Response) => {
-    getAppointmentByID(_req, res);
+export const getAppointmentByIdController = async (
+  req: Request,
+  res: Response
+) => {
+    try{
+        //const id: number = Number(req.params.id);
+        const appointment: IAppointment = await getAppointmentByID(Number(req.params.id));
+        res.status(200).json({
+            success: true,
+            data: appointment,
+        });
+    }
+    catch (error: any) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
 };
 
 // POST
-export const createAppointmentController = async (_req: Request, res: Response) => {
-    createAppointment(_req, res);
+export const createAppointmentController = async (
+  req: Request,
+  res: Response
+) => {
+    try {
+        //const { date, time, userId }: ICreateAppointmentDTO = req.body;
+        const appointment: IAppointment = await createAppointment(req.body);
+        res.status(201).json({
+            success: true,
+            data: appointment,
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
 };
 
 // PUT
-export const CancelAppointmentController = async (_req: Request, res: Response) => {
-    cancelAppointment(_req, res);
-};
+export const CancelAppointmentController = async (
+  req: Request,
+  res: Response
+) => {
+    try {
+        //const id: number = Number(req.params.id);
+        const appointmentID: number = await cancelAppointment(Number(req.params.id));
+        res.status(200).json({
+            success: true,
+            data: appointmentID,
+        });
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        })
+    }
+}
