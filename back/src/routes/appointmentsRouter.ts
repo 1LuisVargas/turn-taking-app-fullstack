7ed34809f5dc6c7262f5fd576481306c2f9b8839
Luis Vargas <luisvargas98@hotmail.com>
2025-05-20 19:37:41 -0500

    Fixed the put route for appointsments

--- back/src/routes/appointmentsRouter.ts
+++ back/src/routes/appointmentsRouter.ts
@@ -13,6 +13,6 @@ const appointmentsRouter: Router = Router();
 appointmentsRouter.get("/", getAppointmentsController);
 appointmentsRouter.get("/:id", getAppointmentByIdController);
 appointmentsRouter.post("/schedule", createAppointmentController);
-appointmentsRouter.put("/cancel", CancelAppointmentController);
+appointmentsRouter.put("/cancel/:id", CancelAppointmentController);
 
 export default appointmentsRouter;
