faeedc4b0a4696fd0b530e6a0cc26fb465a23b4d
Luis Vargas <luisvargas98@hotmail.com>
2025-05-22 19:28:21 -0500

    Configured the tsconfig and data-source

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -1,3 +1,10 @@
 import "dotenv/config";
 
 export const PORT:number = Number(process.env.PORT) || 3000;
+
+// Postgres
+export const DB_HOST: string = process.env.DB_HOST || "localhost";
+export const DB_PORT: number = Number(process.env.DB_PORT);
+export const DB_USERNAME: string = process.env.DB_USERNAME || "test";
+export const DB_PASSWORD: string = process.env.DB_PASSWORD || "test";
+export const DB_NAME: string = process.env.DB_NAME || "test";
