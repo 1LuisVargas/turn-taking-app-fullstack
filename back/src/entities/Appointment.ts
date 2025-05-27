e7151319227d1acaa65b9e79544d8d3f27c1d0a5
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-05-27 18:11:15 -0500

    Fixed the enum column definition

--- back/src/entities/Appointment.ts
+++ back/src/entities/Appointment.ts
@@ -29,7 +29,7 @@ class Appointment {
 
   @Column({
     type: "enum",
-    enum: "appointmentStatus",
+    enum: appointmentStatus,
     default: appointmentStatus.ACTIVE,
   })
   status: appointmentStatus;
