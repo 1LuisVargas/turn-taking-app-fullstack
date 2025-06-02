f1ed6442086edbcfa714777af95312c1277e87c9
Luis Vargas <luisvargas98@hotmail.com>
2025-06-01 21:04:18 -0500

    Finished the initial setup without styling

---
+++ front/module3-project/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Appointments</a>
+            <a href="#">Login</a>
+        </nav>
+    );
+};
+
+export default NavBar;
