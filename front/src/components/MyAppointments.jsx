1d796174904a978e8e521c3cd07c7651c3617f4c
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-06-14 17:35:30 -0500

    Created new component for appointments. New helper folder and empty array inside of it

---
+++ front/src/components/MyAppointments.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import appointments from "../helpers/myAppointments";
+
+const Appointments = () => {
+    return (
+        <div>
+            <h1>My Appointments</h1>
+            const [appointments, setAppointments] = useState([]);
+        </div>
+    );
+};
+
+export default Appointments;
---
+++ front/src/helpers/myAppointments.js
@@ -0,0 +1,25 @@
+const appointments = [
+    {
+        id: 1,
+        date: "2023-06-01",
+        time: "10:00",
+        userID: 1,
+        status: "pending",
+    },
+    {
+        id: 2,
+        date: "2023-06-02",
+        time: "14:00",
+        userID: 2,
+        status: "pending",
+    },
+    {
+        id: 3,
+        date: "2023-06-03",
+        time: "16:00",
+        userID: 3,
+        status: "pending",
+    },
+];
+
+export default appointments;
