9cfc70b823f48b2d892ce6d4c30e5682a4b965e8
Luis Vargas <simplelogin-newsletter.14a9x@simplelogin.com>
2025-07-04 09:02:43 -0500

    Added Cors usage to index.ts route

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usersRouter from "./usersRouter";
 import appointmentsRouter from "./appointmentsRouter";
+import cors from 'cors';
 
 const router: Router = Router();
 router.use("/users", usersRouter);
 router.use("/appointments", appointmentsRouter);
+router.use(cors());
 
 export default router;
